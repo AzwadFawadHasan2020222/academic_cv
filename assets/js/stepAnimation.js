124d3801197ddf0c6f71ac5c96b75799c74dc60f
AzwadFawadHasan2020222 <106537814+AzwadFawadHasan2020222@users.noreply.github.com>
2024-08-18 20:21:15 +0600

    fixed pub section

--- assets/js/stepAnimation.js
+++ assets/js/stepAnimation.js
@@ -5,3 +5,21 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
 
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  let horizontalSectionCoding = document.querySelector('.horizontal-coding');
+  
+  gsap.to('.horizontal-coding', {
+    x: () => horizontalSectionCoding.scrollWidth * -1,
+    xPercent: 100,
+    scrollTrigger: {
+      trigger: '.horizontal-coding',
+      start: 'center center',
+      end: '+=2000px',
+      pin: '#horizontal-scroll-coding',
+      scrub: true,
+      invalidateOnRefresh: true
+    }
+  });
+  
